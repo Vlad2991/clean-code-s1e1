9268bae1e65e899bbb17db8421b3da6a3e42d742
Vlad2991 <vlad29911710@gmail.com>
2024-12-14 15:32:47 +0300

    fix: add media alternative options according to rule 1.2 in advanced-level.md refactor: update CSS according to BEM to rule 2.1 in

--- app.js
+++ app.js
@@ -10,8 +10,8 @@
 
 var taskInput=document.getElementById("new-task");//Add a new task.
 var addButton=document.getElementsByTagName("button")[0];//first button
-var incompleteTaskHolder=document.getElementById("incompleteTasks");//ul of #incompleteTasks
-var completedTasksHolder=document.getElementById("completed-tasks");//completed-tasks
+var incompleteTaskHolder=document.getElementById("incomplete-task");//ul of #incomplete-task
+var completedTasksHolder=document.getElementById("incomplete-task");//completed-tasks
 
 
 //New task list item
@@ -131,7 +131,7 @@ var taskIncomplete=function(){
     console.log("Incomplete Task...");
 //Mark task as incomplete.
     //When the checkbox is unchecked
-    //Append the task list item to the #incompleteTasks.
+    //Append the task list item to the #incomplete-task.
     var listItem=this.parentNode;
     incompleteTaskHolder.appendChild(listItem);
     bindTaskEvents(listItem,taskCompleted);
